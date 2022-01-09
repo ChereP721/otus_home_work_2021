8ae290b0863b6a4c12ec351d6d3df4b0800dd69a
ChereP721 <alexnorin721@gmail.com>
2022-01-09 20:59:13 +0300

    HW4

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,53 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	item, ok := c.items[key]
+	if !ok {
+		return nil, false
+	}
+
+	c.queue.MoveToFront(item)
+	ci, ok := (item.Value).(cacheItem)
+	if !ok {
+		return nil, false
+	}
+
+	return ci.value, true
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	ci := cacheItem{key: key, value: value}
+
+	if item, ok := c.items[key]; ok {
+		item.Value = ci
+		c.queue.MoveToFront(item)
+
+		return true
+	}
+
+	if c.queue.Len() == c.capacity {
+		ci, ok := (c.queue.Back().Value).(cacheItem)
+		if !ok {
+			panic("Cache item is not cacheItem")
+		}
+		delete(c.items, ci.key)
+		c.queue.Remove(c.queue.Back())
+	}
+
+	c.items[key] = c.queue.PushFront(ci)
+
+	return false
 }
 
 type cacheItem struct {
